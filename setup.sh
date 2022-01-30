1d95986c696ff24662556193e38fe7d3f3e6ee20
JonasVerbickas <jonasverbic@gmail.com>
2022-01-30 00:01:07 +0000

    Initial commit

---
+++ setup.sh
@@ -0,0 +1,8 @@
+sudo apt update
+sudo apt upgrade -y
+sudo apt install git
+git config --global user.name "JonasVerbickas"
+git config --global user.email "jonasverbic@gmail.com"
+sudo apt install neovim
+sudo apt install zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
