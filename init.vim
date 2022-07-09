419036cbb30690b205e2ef787329b28ca4285900
JonasVerbickas <jonasverbic@gmail.com>
2022-07-09 18:31:39 +0100

    NVIM setup

---
+++ init.vim
@@ -0,0 +1,7 @@
+:set number relativenumber
+call plug#begin()
+Plug 'projekt0n/github-nvim-theme'
+call plug#end()
+PlugInstall()
+colorscheme github_*
+
--- setup.sh
+++ setup.sh
@@ -1,8 +1,13 @@
-sudo apt update
-sudo apt upgrade -y
+# general stuff
+sudo apt update -y && sudo apt upgrade -y
 sudo apt install git
-git config --global user.name "JonasVerbickas"
-git config --global user.email "jonasverbic@gmail.com"
-sudo apt install neovim
+# terminal
 sudo apt install zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# nvim
+sudo apt install neovim
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+cp init.vim ~/.config/nvim/init.vim
+
+git config --global core.editor "nvim"
